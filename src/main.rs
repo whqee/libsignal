d729a4b513a40bebcf8e859f855d433e651021d1
whqee <whqee@qq.com>
2023-02-18 01:02:09 +0800

    添加方波,三角波测试

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,30 @@ fn main() {
     let cos_sig = signal::TrigSig::new(signal::Type::Cos, freq, amp, offset);
 
     // let tan_sig = signal::Tan::new(freq, amp, offset);
-    let mix_sig = sin_sig.clone() + cos_sig.clone();
+    // let mut mix_sig = sin_sig.clone() + cos_sig.clone();
 
-    let (duration, start, framerate) = (1.5, 0.0, 44100);
+    let (freq, amp, offset) = (1000.0, 0.5, 0.0);
+    let mut mix_sig = signal::TrigSig::new(signal::Type::Sin, freq, amp, offset);
+
+    // // 方波
+    // for i in [
+    //     3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0, 17.0, 19.0, 21.0, 23.0, 25.0, 27.0, 29.0,
+    // ] {
+    //     mix_sig += signal::TrigSig::new(signal::Type::Sin, freq * i, amp / i, offset);
+    // }
+
+    // // 方波
+    // for i in (2..2000).filter(|x| x % 2 != 0) {
+    //     let i = i as f32;
+    //     mix_sig += signal::TrigSig::new(signal::Type::Sin, freq * i, amp / i, offset);
+    // }
+
+    // 三角波
+    for i in (2..200).map(|x| x as f32) {
+        mix_sig += signal::TrigSig::new(signal::Type::Sin, freq * i, amp / i, offset);
+    }
+
+    let (duration, start, framerate) = (2.0, 0.0, 44100);
     let sin_wave = sin_sig.make_wave(duration, start, framerate);
     let cos_wave = cos_sig.make_wave(duration, start, framerate);
     // let tan_wave = tan_sig.make_wave(duration, start, framerate);
@@ -22,8 +43,8 @@ fn main() {
     // let wave_head = [0u8; 44];
     // file.write(&wave_head).unwrap();
 
-    file.write(sin_wave.raw_as_ref()).unwrap();
-    file.write(cos_wave.raw_as_ref()).unwrap();
+    // file.write(sin_wave.raw_as_ref()).unwrap();
+    // file.write(cos_wave.raw_as_ref()).unwrap();
     // file.write(tan_wave.raw_as_ref()).unwrap();
     file.write(mix_wave.raw_as_ref()).unwrap();
 }
